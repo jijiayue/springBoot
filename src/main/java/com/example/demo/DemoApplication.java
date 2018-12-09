a6a91b25ecf99b67a57c31dea321675774ab0183
jijiayue <15275263276yun>
2018-12-09 21:40:55 +0800

    tomcat部署遇到一些坑  和处理

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,27 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 public class DemoApplication {
 
+
+//  部署到tomcat 需要   并且在pom中还要做修改   jdk和tomcat 有版本支持关系
+//	public class DemoApplication extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(DemoApplication.class);
+//	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }

aa730c93490ea73a3ab10fa475338e2986c6b32a
jijiayue <15275263276yun>
2019-01-31 16:36:09 +0800

    war包 部署

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,17 +7,16 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.WebApplicationInitializer;
 
+
+//public class DemoApplication {
+//部署到tomcat 需要   并且在pom中还要做修改   jdk和tomcat 有版本支持关系
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
-
-//  部署到tomcat 需要   并且在pom中还要做修改   jdk和tomcat 有版本支持关系
-//	public class DemoApplication extends SpringBootServletInitializer {
-//
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(DemoApplication.class);
-//	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
 
 
 	public static void main(String[] args) {
